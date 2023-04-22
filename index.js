8bba415a36bfe4db945a643843738fd63b9eea1d
AllanMuhari <allanmuhari087@gmail.com>
2023-04-23 00:28:44 +0300

    js file

--- index.js
+++ index.js
@@ -51,14 +51,16 @@ window.addEventListener('load', () => {
         input.value = '';
 
         task_edit_el.addEventListener('click', () => {
-           if(task_edit_el.innerText.toLowerCase()== 'edit'){
+           if(task_edit_el.innerText.toLowerCase()== 
+           'edit'){
             task_input_el.removeAttribute('readonly');
             task_input_el.focus();
             task_edit_el.innerText='Save';
               }else{
-                console.log('Save')
+                task_input_el.setAttribute('readonly','readonly');
+                task_edit_el.innerText='Edit';
               }
         });
 
-    });
-});
+    })
+})
